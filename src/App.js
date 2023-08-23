fb47be5a2fe09bf425cb333665751dc6bca913c6
Krishna Nigalye <krishna.nigalye@gmail.com>
2023-08-23 18:52:35 +0530

    reusable modal

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Modal from "./Modal";
+
+function App() {
+  return (
+    <>
+      <Modal>
+        <h2>Hello World!</h2>
+        <p>Krishna here!!!!</p>
+      </Modal>
+    </>
+  );
+}
+
+export default App;
